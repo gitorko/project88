90a2250d9588d795ca7f80d3e512cb433d228e39
Arjun Surendra <asurendra@vmware.com>
2021-11-05 18:51:51 +0530

    code

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/project88-ui'),
+      dir: require('path').join(__dirname, './coverage/ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
